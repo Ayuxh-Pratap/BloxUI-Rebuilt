72336624ee4189bfa3a3d941bd5da50004ce6f35
Ayush Pratap Singh <161467009+Ayuxh-Pratap@users.noreply.github.com>
2024-07-22 08:05:40 +0530

    ts include inc + iframe init

---
+++ components/mdx/Ytvideo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Ytvideo() {
+  return (
+    <div>
+        <iframe width="800" height="400" src="https://www.youtube.com/embed/uBSoE7f21hM?si=cKv1QOG9zhYSO-wS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
+  )
+}
